ea114023db1adc1182ce9ea5c7a5bc14896d6bc6
Admin-PC <sambit.swain123@gmail.com>
2020-08-03 12:30:53 +0530

    updated config.js file

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_D8HvKjZlB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5084kl37i4520rmdqabfjvb2i0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'ap-south-1_FiV5Syqpu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2oavaml587sj5giuks93iu9j4v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://f19a0lrmg3.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://ujz75nm0r0.execute-api.ap-southeast-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://ujz75nm0r0.execute-api.ap-southeast-2.amazonaws.com/prod',
     }
 };
